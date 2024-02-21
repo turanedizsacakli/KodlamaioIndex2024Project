1023c0a49c366c98272808603d11314ac7fed445
TURAN EDİZ SAÇAKLI <46419936+turanedizsacakli@users.noreply.github.com>
2024-02-21 19:23:41 +0300

    Add project files.

---
+++ Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Course
+    {
+        public int id;
+        public string name;
+        public string description;
+
+    }
+}
