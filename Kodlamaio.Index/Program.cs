821c91de0d0b13ae74c85cc2a0d9f63fae2e44bf
TURAN EDİZ SAÇAKLI <46419936+turanedizsacakli@users.noreply.github.com>
2024-02-25 00:34:13 +0300

    updated...

--- Kodlamaio.Index/Program.cs
+++ Kodlamaio.Index/Program.cs
@@ -12,45 +12,56 @@ internal class Program
 
         Console.WriteLine("HoşGeldin!\n");
 
-        Console.WriteLine("Kategori Eklemek İster Misin? e/y");
+        Console.WriteLine("Kurs Eklemek İster Misin? e/y");
 
         string answer=Console.ReadLine();
 
         if (answer == "e")
         {
-            
-            _course = new CourseManager(new courseDal());
-
-            Console.WriteLine("Kurs ID Giriniz : ");
-            string _courseId = Console.ReadLine();
-
-            Console.WriteLine("Kurs Adını Giriniz : ");
-            string _courseName = Console.ReadLine();
-
-            Console.WriteLine("Kurs Tanımını Giriniz : ");
-            string _courseDescription = Console.ReadLine();
-
-            Console.WriteLine("Kurs Öğretmen Numarasını Giriniz : ");
-            string _instructorId = Console.ReadLine();
-
-            Console.WriteLine("Kurs Kategori Numarasını Giriniz : ");
-            string _categoryId= Console.ReadLine();
-
-            _course.Add(new Course
-            {
-                CourseId = _courseId,
-                CourseName = _courseName,
-                CourseDescription = _courseDescription,
-                InstructorId = _instructorId,
-                CategoryId = _categoryId
-            });
-
-
-            Console.ReadLine();
-
+            _course = AddCourse();
+            GetList(_course);
         }
 
+        Console.ReadLine();
 
+    }
 
+    private static void GetList(ICourseService _course)
+    {
+        List<Course> _courses = new List<Course>();
+        _courses = _course.GetAll();
+        foreach (var course in _courses)
+        {
+            Console.WriteLine(course.CourseName);
+        }
+    }
+
+    private static ICourseService AddCourse()
+    {
+        ICourseService _course = new CourseManager(new courseDal());
+        Console.WriteLine("Kurs ID Giriniz : ");
+        string _courseId = Console.ReadLine();
+
+        Console.WriteLine("Kurs Adını Giriniz : ");
+        string _courseName = Console.ReadLine();
+
+        Console.WriteLine("Kurs Tanımını Giriniz : ");
+        string _courseDescription = Console.ReadLine();
+
+        Console.WriteLine("Kurs Öğretmen Numarasını Giriniz : ");
+        string _instructorId = Console.ReadLine();
+
+        Console.WriteLine("Kurs Kategori Numarasını Giriniz : ");
+        string _categoryId = Console.ReadLine();
+
+        _course.Add(new Course
+        {
+            CourseId = _courseId,
+            CourseName = _courseName,
+            CourseDescription = _courseDescription,
+            InstructorId = _instructorId,
+            CategoryId = _categoryId
+        });
+        return _course;
     }
 }
